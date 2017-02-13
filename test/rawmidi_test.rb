92ef4bc283cc4a1ff546891399326ab69479756e
Damián Silvani <munshkr@gmail.com>
2017-02-13 11:30:08 -0300

    Remove useless test

--- test/rawmidi_test.rb
+++ test/rawmidi_test.rb
@@ -4,8 +4,4 @@ class RawMIDITest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::RawMIDI::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
